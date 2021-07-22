9dd2e546c3a20cdda1a8b2230e700dfb02774d36
huantingchen <vvvv00006huan@gmail.com>
2021-07-22 16:07:26 -0400

    move Cart-number to header

--- js/layout.js
+++ js/layout.js
@@ -1,1 +1,15 @@
-$( "header" ).load( "layout/header.html" );
+
+
+$( "header" ).load( "layout/header.html",function(){
+    const numberHtml=document.getElementsByClassName("cart-number");
+    let user = $.cookie("user");
+    
+        $.get(` https://catropolis-55dac-default-rtdb.firebaseio.com/User-Cart/${user}/.json`, null, function (res) {
+            console.log(res);
+            let adoptedNumber=Object.keys(res).length;
+            numberHtml[0].innerHTML=`<a href="cart.html" target="_blank"><div class="cart"><i class="bi bi-cart-fill"></i> Cart ${adoptedNumber}</div></a>`;
+        });
+   
+} )
+
+
--- js/list.js
+++ js/list.js
@@ -145,7 +145,7 @@ function renderCartInfo(){
     $.get(` https://catropolis-55dac-default-rtdb.firebaseio.com/User-Cart/${user}/.json`, null, function (res) {
         console.log(res);
         let adoptedNumber=Object.keys(res).length;
-        numberHtml[0].innerHTML=`<a href="cart.html?id=${id}" target="_blank"><div class="cart"><i class="bi bi-cart-fill"></i> Adopted ${adoptedNumber}</div></a>`;
+        numberHtml[0].innerHTML=`<a href="cart.html?id=${id}" target="_blank"><div class="cart"><i class="bi bi-cart-fill"></i> Cart ${adoptedNumber}</div></a>`;
     });
 };
 
