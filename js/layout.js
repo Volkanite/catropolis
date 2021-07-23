4d5729f49f789382de8104e149f7bf7e819d9678
huantingchen <vvvv00006huan@gmail.com>
2021-07-23 08:49:51 -0400

    fix: show zero cart-number immediate once log in

--- js/layout.js
+++ js/layout.js
@@ -5,7 +5,12 @@ $( "header" ).load( "layout/header.html",function(){
     
         $.get(` https://catropolis-55dac-default-rtdb.firebaseio.com/User-Cart/${user}/.json`, null, function (res) {
             console.log(res);
-            let adoptedNumber=Object.keys(res).length;
+            let adoptedNumber=0;
+        if(res==null){
+            adoptedNumber=0;
+        }else{
+            adoptedNumber=Object.keys(res).length;
+        }
             numberHtml[0].innerHTML=`<a href="cart.html" target="_blank"><div class="cart"><i class="bi bi-cart-fill"></i> Cart ${adoptedNumber}</div></a>`;
         });
    
--- js/list.js
+++ js/list.js
@@ -144,7 +144,12 @@ $(document).ready(function(){
 function renderCartInfo(){
     $.get(` https://catropolis-55dac-default-rtdb.firebaseio.com/User-Cart/${user}/.json`, null, function (res) {
         console.log(res);
-        let adoptedNumber=Object.keys(res).length;
+        let adoptedNumber=0;
+        if(res==null){
+            adoptedNumber=0;
+        }else{
+            adoptedNumber=Object.keys(res).length;
+        }
         numberHtml[0].innerHTML=`<a href="cart.html?id=${id}" target="_blank"><div class="cart"><i class="bi bi-cart-fill"></i> Cart ${adoptedNumber}</div></a>`;
     });
 };
