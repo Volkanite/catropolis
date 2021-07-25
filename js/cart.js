Volkanite/catropolis

commit c73f893ebf84f478800c1559cd45f266ea7f4617

Cart functions moved to separate JavaScript file so they can be easily referenced from other pages.

js/cart.js
@@ -1,0 +1,65 @@
+//----------------Cart--------------------
+
+let adoptedNumber=0;
+const numberHtml=document.getElementsByClassName("cart-number");
+let id=[];
+
+
+//Delete it after sign-in system built
+$(document).ready(function(){
+    if(!$.cookie("user")||$.cookie("user")===null){
+        let user=prompt("Please enter User name","Type your name here");
+        $.cookie("user",user);
+    }
+});
+
+
+function renderCartInfo(){
+    let user = $.cookie("user");
+    $.get(` https://catropolis-55dac-default-rtdb.firebaseio.com/User-Cart/${user}/.json`, null, function (res) {
+        adoptedNumber=Object.keys(res).length;
+       
+        numberHtml[0].innerHTML=`<a href="cart.html" target="_blank"><div class="cart"><i class="bi bi-cart-fill"></i> Cart ${adoptedNumber}</div></a>`;
+        
+    });
+};
+
+
+function adoptThisCat(adoptedItemBtn){ 
+    let id_no=Number(adoptedItemBtn.dataset.id);
+    let catID=(id_no+(currentPage-1)*8);
+    let x=0;
+    let user = $.cookie("user");
+
+    function sendToDB(){
+        let data={
+            id:catID
+        };
+        firebase.database().ref("User-Cart/" + user ).push(data, function (error) {    //set丟指定的資料; push自動給一個key值 ;data後面的func是 callback function,會在前面資料拿到之後才執行
+            if (error) {
+                console.log(error); 
+            } else {
+                alert('The Cat Is Added To Your Adoption-Cart!  Click Your Cart To Check Cats You Adopt.');
+            }
+            renderCartInfo();
+        }) 
+    }
+    
+    id.forEach(function(id){
+        if(id==catID){
+            return;
+        }else{
+            x++;
+        };
+    })
+
+    if(x==id.length){
+        console.log(catID);
+        id.push(catID);
+        sendToDB();
+
+    }else{
+        alert('This Cat is Already In The Cart.');
+        return;
+    }  
+}

js/list.js
@@ -123,13 +123,12 @@ readJSONFile("./seeders/item.json")
 
 
 
-
-//----------------Cart
+/*
+//----------------Cart--------------------
 
 let adoptedNumber=0;
 const numberHtml=document.getElementsByClassName("cart-number");
 let id=[];
-
 
 
 //Delete it after sign-in system built
@@ -183,7 +182,6 @@ function adoptThisCat(adoptedItemBtn){
     if(x==id.length){
         console.log(catID);
         id.push(catID);
-        debugger;
         sendToDB();
 
     }else{
@@ -192,7 +190,7 @@ function adoptThisCat(adoptedItemBtn){
     }  
 }
 
-
+*/
 
 
 

index.html
@@ -10,6 +10,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script> <!--for cookies-->
     <script defer src="js/list.js"></script>
     <script defer src="js/slider.js"></script>
+    <script defer src="js/cart.js"></script>
     <script defer src="js/layout.js"></script>
     <link rel="stylesheet" type="text/css" href="css/list.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">    

commit 21c65107695ae6b3d9525259e19692378f2067dc

fix undefined value:currentPage caused by c73f893

cat.html
@@ -9,6 +9,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/list.css"/>
     <script defer src="js/layout.js"></script>
+    <script defer src="js/cart.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     
 
@@ -54,7 +55,7 @@
                 item_html += '<div><span class=item_name>' + item.name + '</span>';
                 item_html += '<span class=item_type>' + item.type + '</span></div>';
                 item_html += '<button class="listen-btn" onclick="playCatSound()" data-id="' + id + '"> Listen </button>';
-                item_html += '<button class="adopt-btn" onclick="adoptThisCat(this)" data-id="' + id + '"> Adopt </button>';
+                item_html += '<button class="adopt-btn" onclick="adoptThisCat(this,currentPage)" data-id="' + id + '"> Adopt </button>';
                 item_html += '<div class=item_body>';
                 item_html += '<div class=item_attr><ul><li>age:' + item.age + '</li><li>' + item.gender + '</li><li>' + item.description + '</li></ul></div>';
                 item_html += '</div>';

js/cart.js
@@ -25,7 +25,7 @@ function renderCartInfo(){
 };
 
 
-function adoptThisCat(adoptedItemBtn){ 
+function adoptThisCat(adoptedItemBtn, currentPage){ 
     let id_no=Number(adoptedItemBtn.dataset.id);
     let catID=(id_no+(currentPage-1)*8);
     let x=0;

js/list.js
@@ -94,7 +94,7 @@ function layoutItems(items){
         item_html+='<span class=item_type>'+item.type+'</span></div>';
         item_html+='<div class=item_body>';
         item_html+='<div class=item_attr><ul><li>age:'+item.age+'</li><li>'+item.gender+'</li><li>'+item.description+'</li></ul></div>';
-        item_html+='<button class="adopt-btn" onclick="adoptThisCat(this)" data-id="'+ (id++) +'"> Adopt </button>';
+        item_html+='<button class="adopt-btn" onclick="adoptThisCat(this,currentPage)" data-id="'+ (id++) +'"> Adopt </button>';
         item_html+='</div>';
         item_html+='</div>';
 